f1e825ddcd871c7d706ac40825480751e67a8ac0
Dayal <dc7solanki@gmail.com>
2024-08-09 12:07:27 +0530

    this will give your know about Accordian by react only

---
+++ accordian-by-react/src/components/data.js
@@ -0,0 +1,23 @@
+const data=[
+    {
+        id:'1',
+        question:'what is your first name?',
+        answer:"Dayal"
+    },
+    {
+        id:'2',
+        question:'what is your middle name?',
+        answer:"Chand"
+    },
+    {
+        id:'3',
+        question:'what is your last name?',
+        answer:"Solanki"
+    },
+    {
+        id:'4',
+        question:'what is your full name?',
+        answer:"Dayal Chand Solanki"
+    }
+]
+    export default data
